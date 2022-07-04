3dd9a29dd97bf94ae18a4cd7c80e7d44fd91cb7c
DivinIrakiza <irakizadivin@gmail.com>
2022-07-04 11:35:38 +0200

    initial commit

--- src/Account.h
+++ src/Account.h
@@ -1,16 +1,159 @@
 #include <string>
-
+#include <cstdlib>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class Account
 {
 private:
-    string bankName;
     int accountNumber;
-    char accountHolder[100];
-    double accountBalance;
+    char *fullName;
+    float accountBalance;
+    int generateAccountNumber();
 
 public:
-    Account();
+    Account(){};
+    Account(char *fullName, float balance);
     void createAccount();
+    void setAccountNumber(int accountNumber);
+    void setFullName(char *fullName);
+    void setAccountBalance(float accountBalance);
+    Account getAccount(int accountNumber);
+    vector<Account> getAccounts();
+    string extractorToAccount(string line, string prop);
+    char *getFullName();
+    void makeDeposit(int accountNumber, float deposit);
+};
+
+Account::Account(char *fullName, float balance)
+{
+    this->accountNumber = generateAccountNumber();
+    this->fullName = fullName;
+    this->accountBalance = balance;
+}
+
+int Account::generateAccountNumber()
+{
+    return 10000 + (rand() % 100000);
+}
+
+char *Account::getFullName()
+{
+    return this->fullName;
+}
+
+void Account::createAccount()
+{
+    ofstream fout("db.txt", fstream::in | fstream::out | fstream::app);
+    fout << "accountNumber:" << this->accountNumber << "|"
+         << "fullName:" << this->fullName << "|"
+         << "accountBalance:" << this->accountBalance << "|" << endl;
+
+    cout << "Account created successfully!" << endl;
+}
+
+Account Account::getAccount(int accountNumber)
+{
+    ifstream inFile("db.txt");
+
+    string line;
+
+    Account account;
+    while (inFile >> line)
+    {
+
+        string test = to_string(accountNumber);
+        if (line.find(test) != string::npos)
+        {
+            cout << "AccountNumber: " << extractorToAccount(line, "accountNumber") << endl;
+
+            cout << "AccountName: " << extractorToAccount(line, "fullName") << endl;
+            cout << "AccountBalance: " << extractorToAccount(line, "accountBalance") << endl;
+
+            account.setAccountNumber(stoi(extractorToAccount(line, "accountNumber")));
+            account.setFullName(&extractorToAccount(line, "fullName")[0]);
+            account.setAccountBalance(stoi(extractorToAccount(line, "accountBalance")));
+        }
+    }
+
+    return account;
+}
+
+vector<Account> Account::getAccounts()
+{
+    ifstream inFile("db.txt");
+
+    string word;
+
+    vector<Account> accounts;
+    while (inFile >> word)
+    {
+        Account account;
+        cout << "AccountNumber: " << extractorToAccount(word, "accountNumber") << endl;
+        cout << "AccountName: " << extractorToAccount(word, "fullName") << endl;
+        cout << "AccountBalance: " << extractorToAccount(word, "accountBalance") << endl;
+
+        account.setAccountNumber(stoi(extractorToAccount(word, "accountNumber")));
+        account.setFullName(&extractorToAccount(word, "fullName")[0]);
+        account.setAccountBalance(stoi(extractorToAccount(word, "accountBalance")));
+
+        accounts.push_back(account);
+        cout << endl;
+    }
+
+    return accounts;
+}
+
+string Account::extractorToAccount(string line, string prop)
+{
+    size_t found = line.find(prop);
+
+    string strNew;
+    if (found != string::npos)
+    {
+        string test = line.substr((found + prop.length()));
+        unsigned first = test.find(":");
+        unsigned last = test.find("|");
+        strNew = test.substr(first + 1, (last - first) - 1);
+    }
+    return strNew;
+}
+
+void Account::makeDeposit(int accountNumber, float deposit)
+{
+    Account account = this->getAccount(accountNumber);
+
+    vector<Account> accounts = this->getAccounts();
+    cout << "Size: " << this->getAccounts().size() << endl;
+
+    ofstream fout("db.txt");
+
+    for (Account _account : accounts)
+    {
+        float udpated = _account.accountBalance;
+        if (_account.accountNumber == accountNumber)
+            udpated += deposit;
+
+        cout << "My name is: " << _account.fullName << endl;
+
+        fout << "accountNumber:" << _account.accountNumber << "|"
+             << "fullName:" << _account.fullName << "|"
+             << "accountBalance:" << udpated << "|" << endl;
+    }
+}
+
+void Account::setAccountNumber(int accountNumber)
+{
+    this->accountNumber = accountNumber;
+}
+
+void Account::setFullName(char *fullName)
+{
+    this->fullName = fullName;
+}
+
+void Account::setAccountBalance(float balance)
+{
+    this->accountBalance = balance;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,117 @@
 #include <iostream>
+#include "Account.h"
 
 using namespace std;
 
+void displayBanner()
+{
+    cout << "------------------------------------------------" << endl;
+    cout << "\tWELCOME TO BANKING SYSTEM" << endl;
+    cout << "------------------------------------------------" << endl;
+}
+
+void createAccountMenu()
+{
+    cout << "1. Create an account." << endl;
+    char name[100];
+
+    cout << "Enter your fullName: ";
+    cin.ignore();
+    cin.getline(name, 100);
+
+    float deposit;
+    cout << "Your first deposit: ";
+    cin >> deposit;
+
+    Account account = Account(name, deposit);
+    account.createAccount();
+
+    cout << "Details " << account.getFullName() << endl;
+}
+
+void displayAllAccounts()
+{
+    cout << "Account " << endl;
+    Account account;
+    account.getAccounts();
+}
+
+void depositMenu()
+{
+    int accountNumber;
+    float amount;
+    cout << "Enter your account Number: ";
+    cin >> accountNumber;
+
+    cout << "Amount: ";
+    cin >> amount;
+
+    Account account;
+    account.makeDeposit(accountNumber, amount);
+}
+void displayAccount()
+{
+    int accountNumber;
+    cout << "Account Number: ";
+    cin >> accountNumber;
+    Account account;
+    account.getAccount(accountNumber);
+}
+
 int main()
 {
-    cout << "Testing" << endl;
+    srand((unsigned)time(0));
+
+    displayBanner();
+    cout << "Choose(1-9): " << endl;
+    cout << "\t1. Create an account. " << endl;
+    cout << "\t2. Display Account details. " << endl;
+    cout << "\t3. Modify Account Details " << endl;
+    cout << "\t4. Close your Account. " << endl;
+    cout << "\t5. Make a Deposit " << endl;
+    cout << "\t6. Make a withdraw " << endl;
+    cout << "\t7. View all accounts " << endl;
+    cout << "\t8. Check Balance " << endl;
+    cout << "\t9. Exit" << endl;
+
+    int choice;
+
+    cout << "Your Choice(1-9): ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        system("clear");
+        displayBanner();
+        createAccountMenu();
+
+        break;
+
+    case 2:
+        system("clear");
+        displayBanner();
+        displayAccount();
+
+        break;
+
+    case 5:
+        system("clear");
+        displayBanner();
+        depositMenu();
+
+        break;
+
+    case 7:
+        system("clear");
+        displayBanner();
+        displayAllAccounts();
+
+        break;
+
+    default:
+        break;
+    }
+
     return 0;
 }
